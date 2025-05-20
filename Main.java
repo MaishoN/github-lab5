f510126706badf22f997868d84a4b1177a00cd1e
maishon <fatnevm29885@gmail.com>
2025-05-20 13:22:37 +0300

    added Main.java - dev

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ class Main{
         System.out.println("коммит 1 в dev");
         System.out.println("коммит 2 в dev");
         System.out.println("коммит 3 в dev");
+        System.out.println("коммит 4 в dev");
     }
 }
